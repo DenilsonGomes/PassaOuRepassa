2f3357e68859fa1af6ea71d72802ca16f009df7c
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-03-23 22:54:27 -0300

    Codigo revisado, botao reset adicionado, pinos reorganizados.

--- passaOuRepassa.ino
+++ passaOuRepassa.ino
@@ -15,11 +15,14 @@
 int botAzul = 2; //Botao da equipe Azul
 int botVermelho = 3; //Botao da equipe Vermelho
 int ledAzul = 4; //Variavel para led azul
-int buzzer = 5; //Variavel para sinal sonoro
-int ledVermelho = 6; //Variavel para led vermelho
-int incrementAzul = 7; //Botao para incrementar pontuação dos azuis
-int reseta = 8; //Botao para resetar a pontuação
+int incrementAzul = 5; //Variavel para led azul
+int buzzer = 6; //Variavel para sinal sonoro
+
+const int DIO = 7; //Define o pino 8 para o sinal digital
+const int CLK = 8; //Define o pino 9 para o clock
+
 int incrementVermelho = 9; //Botao para incrementar pontuação dos vermelhos
+int ledVermelho = 10;; //Variavel para led vermelho
 
 int pontosAzul = 0; //Variavel para pontuação da equipe Azul
 int pontosVermelho = 0; //Variavel para pontuação da equipe Vermelha
@@ -29,9 +32,6 @@ volatile int som; //Variavel para controlar o sinal sonoro
 volatile int estVermelho; //Variavel para controlar o sinal visual da equipe Vermelha
 volatile int estAzul; //Variavel para controlar o sinal visual da equipe Azul
 
-const int DIO = 10; //Define o pino 8 para o sinal digital
-const int CLK = 11; //Define o pino 9 para o clock
-
 // -- Instancia de objetos --
 TM1637Display display(CLK, DIO);  //Inicializa objeto displays com CLK e DIO
 
@@ -40,12 +40,11 @@ void setup() {
   Serial.begin(9600);
   
   display.setBrightness(0x0a);  //Determina o brilho dos leds do minino (0x00) ate o maximo (0xff)
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, INPUT);
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
+  pinMode(ledAzul, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(incrementAzul, INPUT);
+  pinMode(incrementVermelho, INPUT);
   
   // Sintaxe das interrupções:
   // attachInterrupt(numero_interrupt,funcao_a_executar,modo);
@@ -70,8 +69,9 @@ void azul() {
 void loop() {
   uint8_t segto = 0x80; // Atribui os dois pontos a variavel
   display.showNumberDecEx(pontosAzul*1000 + pontosVermelho); //Exibe a pontuação
-
+ 
   if(som){ //Caso algum participante aperte o botão
+//    noInterrupts(); //Desativa as interrupções
     Serial.println("Alarme ativado"); //Aciona o sinal sonoro
 
     if(estAzul && estVermelho){ //Pro caso de as duas interrupções executarem
@@ -86,25 +86,20 @@ void loop() {
     }
     digitalWrite(ledVermelho,estVermelho);
     digitalWrite(ledAzul,estAzul);
-    tone(5,166);
-//  delay(tempo);
+    tone(buzzer,166);
 
     for (int i=tempo/1000;i>=0;i--){
       display.showNumberDecEx(i); //Exibe o tempo
       delay(1000);
     }
     
-    estVermelho = 0;
-    estAzul = 0;
-    som = 0;
-    digitalWrite(ledVermelho,estVermelho);
-    digitalWrite(ledAzul,estAzul);
-    noTone(5);
+    noTone(buzzer);
     Serial.println("Tempo para resposta esgotado!!");
 
     int a = digitalRead(incrementAzul);
     int v = digitalRead(incrementVermelho);
     Serial.println("Esperando pelo botão que diz qual equipe pontuou!");
+    
     while(!a && !v){
       a = digitalRead(incrementAzul);
       v = digitalRead(incrementVermelho);
@@ -117,6 +112,13 @@ void loop() {
       Serial.println("Equipe Vermelha pontuou!");
       pontosVermelho++;
     }
+
+    estVermelho = 0;
+    estAzul = 0;
+    som = 0;
+    digitalWrite(ledVermelho,estVermelho);
+    digitalWrite(ledAzul,estAzul);
+    
     interrupts();
   }
   
